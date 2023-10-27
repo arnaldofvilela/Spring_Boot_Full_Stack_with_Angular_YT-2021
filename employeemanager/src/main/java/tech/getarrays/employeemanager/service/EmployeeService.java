91c27dd2f443f83536b3192689660d9564e0ddb1
Arnaldo Neto <vilela-neto@hotmail.com>
2023-10-27 17:12:26 -0300

    Finished Exception Handling 38:49 min

--- employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package tech.getarrays.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.repo.EmployeeRepo;
 
